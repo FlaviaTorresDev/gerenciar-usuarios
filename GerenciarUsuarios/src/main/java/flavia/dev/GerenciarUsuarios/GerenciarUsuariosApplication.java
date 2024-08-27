348dcbd66dceeb67043990c97cb585020cab3a10
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-27 10:49:34 -0300

    inicializando o projeto, configurando banco de dados e pacotes

---
+++ GerenciarUsuarios/src/main/java/flavia/dev/GerenciarUsuarios/GerenciarUsuariosApplication.java
@@ -0,0 +1,13 @@
+package flavia.dev.GerenciarUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarUsuariosApplication.class, args);
+	}
+
+}
