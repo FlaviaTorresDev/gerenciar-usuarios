be495bd7638bb929153d5513ec23d85e046a7880
FlaviaTorresDev <flaviatorresdev@gmail.com>
2024-08-27 15:42:23 -0300

    Configurações da classe de Utils

--- GerenciarUsuarios/src/main/java/flavia/dev/GerenciarUsuarios/config/JWTAuthFilter.java
+++ GerenciarUsuarios/src/main/java/flavia/dev/GerenciarUsuarios/config/JWTAuthFilter.java
@@ -14,6 +14,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import flavia.dev.GerenciarUsuarios.service.JWTUtils;
+
 import java.io.IOException;
 
 @Component
